5b6904e479fa4507e2735e87a4b4fd50a0e928fb
sujal <sujal.mistry104@gmail.com>
2025-02-10 21:00:06 +0530

    changes for resume and name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Mistry Sujal</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Software Engineer</h5>
         <CTA />
         <HeaderSocials />
 
@@ -27,7 +27,7 @@ const Header = () => {
             </div> */}
 
         <a href="#contact" className="scroll__down">
-          <FaArrowDownLong/>
+          <FaArrowDownLong />
         </a>
       </div>
     </header>
